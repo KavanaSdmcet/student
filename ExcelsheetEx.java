bef9abb89fe44bd8594fbaef08fb06ad4dd1e27d
KavanaSdmcet <163529351+KavanaSdmcet@users.noreply.github.com>
2024-10-23 07:20:48 +0530

    Create ExcelsheetEx.java

---
+++ ExcelsheetEx.java
@@ -0,0 +1,41 @@
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class DBMS {
+    public static void main(String[] args) {
+        String excelFilePath = "example.xlsx";
+        try (FileInputStream fis = new FileInputStream(excelFilePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            // Get the first sheet
+            Sheet sheet = workbook.getSheetAt(0);
+
+            // Iterate through each row
+            for (Row row : sheet) {
+                // Iterate through each cell in the row
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        default:
+                            System.out.print("Unknown value\t");
+                            break;
+                    }
+                }
+                System.out.println();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
